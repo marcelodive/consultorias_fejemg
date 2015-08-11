be50e5149999f94688133613820f043b6e54dbd8
mmiguelcj <mmiguelcj@gmail.com>
2015-08-11 13:45:33 -0300

    Correção do campo ramos para a opção todas as cidades.

--- resources/views/pages/consultorias.blade.php
+++ resources/views/pages/consultorias.blade.php
@@ -60,29 +60,12 @@
 	$(document).ready(function(){	
 		
 		$('#popup').bPopup();
-			
-		var load = $('#fieldsData').data('load');
+	
+		changeFields();
+		//Primeira carregamento dos serviços.	
+		var load = $('#servicesData').data('load');
 		var lists = "";
-		var names = [];
-		var uniqueNames = [];
-		lists = lists + "<option value=\"Todos os Ramos\">Todos os Ramos</option>";
-		for(var city in load){
-			load[city].forEach(function(entry){
-				names.push(entry.name);        		
-		    });
-		}
-		$.each(names, function(i, el){
-		    if($.inArray(el, uniqueNames) === -1) {
-		    	 lists = lists + "<option value=\""+el+"\">"+el+"</option>";
-		   		 uniqueNames.push(el);
-		    }
-		});	
-		$('#fields').html(lists);
-		
-
-		load = $('#servicesData').data('load');
-		lists = "";
-// 		lists = lists + "<option value=\"Todos os Serviços\">Todos os Serviços</option>";
+	  //lists = lists + "<option value=\"Todos os Serviços\">Todos os Serviços</option>";
 		for(var field in load){
 			load[field].forEach(function(entry){
         		lists = lists + "<option value=\""+entry.name+"\">"+entry.name+"</option>";
@@ -95,26 +78,23 @@
 		load = $('#fieldsData').data('load');
 	    var value = $(this).val();
 	    lists = "";
-	    for(var city in load){
-	      if(value == city){
-	        load[city].forEach(function(entry){
-	          lists = lists + "<option value=\""+entry.name+"\">"+entry.name+"</option>";
-	        });
-	      }
-	      else if (value == "Todas as Cidades"){
-	    	  load[city].forEach(function(entry){
-    			  lists = "";
-    			  lists = lists + "<option value=\"Todos os Ramos\">Todos os Ramos</option>";
-		          lists = lists + "<option value=\""+entry.name+"\">"+entry.name+"</option>";
-		      });
-	      }
+	    if (value == "Todas as Cidades"){
+	    	  changeFields();
+	      }else{
+	      	for(var city in load){
+	      		if(value == city){
+	        		load[city].forEach(function(entry){
+	          		lists = lists + "<option value=\""+entry.name+"\">"+entry.name+"</option>";
+	        		});
+	      		}
+			}
+			$('#fields').html(lists);
 	    }
-	    $('#fields').html(lists);
 	    changeServices();
 	  });
 	  
 
-	  $('#fields' ||'#cities').change(function(){
+	  $('#fields').change(function(){
 		  /*load = $('#servicesData').data('load');
 		    var value = $(this).val();
 		    lists = "";
@@ -171,7 +151,28 @@
 			}).then(function(data) {
        			$('#services').html(data);
 				 });
-  	};	
+  	};
+  	
+  	function changeFields(){
+		var load = $('#fieldsData').data('load');
+		var lists = "";				
+		var names = [];
+		var uniqueNames = [];
+		lists = lists + "<option value=\"Todos os Ramos\">Todos os Ramos</option>";
+		for(var city in load){
+			load[city].forEach(function(entry){
+				names.push(entry.name);        		
+		    });
+		}
+
+		$.each(names, function(i, el){
+		    if($.inArray(el, uniqueNames) === -1) {
+		    	 lists = lists + "<option value=\""+el+"\">"+el+"</option>";
+		   		 uniqueNames.push(el);
+		    }
+		});	
+		$('#fields').html(lists);
+	}
 });
 </script>
 @stop
